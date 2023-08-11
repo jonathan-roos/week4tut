bf2e52bc8c7ba95ac943725442e3c5102995df1a
Jonathan Roos <jonathanroos60@gmail.com>
2023-08-11 14:20:33 +1000

    added routes. added login form. added FormsModule to app.modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AccountComponent } from './account/account.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'account', component:AccountComponent },
+  {path: 'login', component:LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  email = "";
+  pwd = "";
 
+  formSubmitted() {
+    alert("Hi");
+  };
 }
