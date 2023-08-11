3ea26d3af85c30e5a4a5fd747ae3e765dc5a7a65
Jonathan Roos <jonathanroos60@gmail.com>
2023-08-11 15:30:52 +1000

    updated readME

--- README.md
+++ README.md
@@ -2,6 +2,24 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.1.8.
 
+## List of Commands Used
+### Angular
+* ng new my-app
+* ng build
+* ng serve --open
+* ng generate component "login"
+* ng generate component "account"
+
+### NPM
+* npm install -g @angular/cli
+* npm install bootstrap --save
+
+### Git
+* git remote add https://github.com/jonathan-roos/week4tut
+* git add . 
+* git commit -m "message"
+* git push origin main
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
@@ -25,3 +43,4 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  email = "";
-  pwd = "";
+  email:string = "";
+  pwd:string = "";
+  error:string ="";
 
-  formSubmitted() {
-    alert("Hi");
+  users = [
+    {email:"test@123.com", pwd:"123"},
+    {email:"test@456.com", pwd:"456"},
+    {email:"test@789.com", pwd:"789"}
+  ];
+
+  constructor(private router:Router) {};
+
+  login() {
+    const loginCredentials = this.users.find(
+      user => user.email === this.email && user.pwd === this.pwd
+    );
+
+    if (loginCredentials) {
+      this.router.navigate(['/account']);
+    } else {
+      this.error = "Invalid Credentials"
+      alert(this.error);
+      this.email="";
+      this.pwd="";
+    };
   };
 }
